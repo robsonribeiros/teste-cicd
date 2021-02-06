04363810a5df182161b67bd5bf937a3fd1939e07
Robson Ribeiro <robsont36@gmail.com>
2021-02-05 21:02:13 -0300

    mudando mensagem

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json({msg: 'Olá Mundo!'})
+  res.json({msg: 'Olá Mundo do Docker!'})
 })
 
 app.listen(process.env.PORT, () => {
